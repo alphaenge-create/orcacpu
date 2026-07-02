1ccc5f890204c77bb26cdd72f6a387502222e939
alphaenge-create <alphaengeserv@gmail.com>
2026-07-02 11:14:13 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-PROJETO",
-  storageBucket: "SEU-PROJETO.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI",
+  apiKey: "AIzaSyBXeXk4T0IlANh1Y8SHX5x_q6Fx9PaCyrY",
+  authDomain: "alphaorc-d3667.firebaseapp.com",
+  projectId: "alphaorc-d3667",
+  storageBucket: "alphaorc-d3667.firebasestorage.app",
+  messagingSenderId: "686963355753",
+  appId: "1:686963355753:web:a9c0568c2ca486b9c932f8"
 };
 
 export const app = initializeApp(firebaseConfig);
