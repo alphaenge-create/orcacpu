209f65c7541d83cc7de046e47f31a36c2c4fa528
alphaenge-create <alphaengeserv@gmail.com>
2026-07-02 10:09:15 -0300

    Fix duplicate Firestore initialization
    Removed duplicate initialization of Firestore database.

--- src/firebase.js
+++ src/firebase.js
@@ -12,6 +12,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
